59810d40c4e76be34caa94c8370c1af156a20f6f
alexseymourdev <alexseymourdev@gmail.com>
2021-03-04 22:19:29 +0000

    adding JS to the compiler

--- .eleventy.js
+++ .eleventy.js
@@ -68,6 +68,7 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("js");
 
   /* Markdown Overrides */
   let markdownLibrary = markdownIt({
