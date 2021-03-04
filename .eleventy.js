d519cafeba3879196e58120d92b4595e3b44db7f
alexseymourdev <alexseymourdev@gmail.com>
2021-03-04 22:20:03 +0000

    Revert "adding JS to the compiler"
    This reverts commit 59810d40c4e76be34caa94c8370c1af156a20f6f.

--- .eleventy.js
+++ .eleventy.js
@@ -68,7 +68,6 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
-  eleventyConfig.addPassthroughCopy("js");
 
   /* Markdown Overrides */
   let markdownLibrary = markdownIt({
